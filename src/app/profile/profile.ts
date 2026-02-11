87644b8f25037f4f174eea26aa8b1a621572323c
abdelllatif <haissouneabdellatif749@gmail.com>
2026-02-11 11:40:51 +0100

    add user profil ts

--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -1,11 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { User } from '../core/models/User';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './profile.html',
-  styleUrl: './profile.scss',
+  styleUrl: './profile.css',
 })
 export class Profile {
 
+  user: User | null = null;
+
+  constructor(private router: Router) {
+    this.loadUser();
+  }
+
+  loadUser() {
+    const userData = localStorage.getItem('user');
+    if (userData) {
+      this.user = JSON.parse(userData);
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.router.navigate(['/login']);
+  }
 }

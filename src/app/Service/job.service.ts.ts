c702b4f2d1339245179b966a487cba226a1b7142
abdelllatif <haissouneabdellatif749@gmail.com>
2026-02-11 15:22:06 +0100

    add service for  fetching jobs

---
+++ src/app/Service/job.service.ts.ts
@@ -0,0 +1,22 @@
+// core/services/job.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JobService {
+
+  private baseUrl = 'https://www.themuse.com/api/public';
+
+  constructor(private http: HttpClient) {}
+
+  getJobs(page: number = 1): Observable<any> {
+    return this.http.get(`${this.baseUrl}/jobs?page=${page}`);
+  }
+
+  getJobById(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/jobs/${id}`);
+  }
+}
